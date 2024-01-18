1e0efd0813fae7a53388d862b62d97bcc6b19b55
Gaurav thakur <gauravthakur827877@gmail.com>
2024-01-18 17:08:35 +0530

    frist commit

---
+++ lib/Homepage.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Demo app'),
+        backgroundColor: Colors.greenAccent.shade100,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.notification_add),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.account_circle),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Image(
+              image: NetworkImage(
+                'https://images.unsplash.com/photo-1536657689560-671d0d8b5f07?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+              ),
+              width: 500,
+              height: 300,
+            ),
+            const SizedBox(height: 10),
+            Align(
+              alignment: Alignment.center,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.favorite),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.comment),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.share),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Align(
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                'Explore the beauty of nature!',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
